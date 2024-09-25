1b92cd0581568f46cf2de60b1ac30d75f89ee8eb
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-09-24 19:47:15 -0500

    first commit

---
+++ src/test/java/com/pubg/api/PubgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pubg.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubgApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
