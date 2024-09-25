4349b40aa5725f718f0672ab5e62668f150eccc2
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-09-25 18:01:40 -0500

    Modeled some of the Map class

--- src/main/java/com/pubg/api/commons/entities/Map.java
+++ src/main/java/com/pubg/api/commons/entities/Map.java
@@ -1,4 +1,22 @@
 package com.pubg.api.commons.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "maps")
 public class Map {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    
 }
