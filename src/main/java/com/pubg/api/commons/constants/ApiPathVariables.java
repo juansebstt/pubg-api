79d86dce3f2ef815827b7aa7d63f5d9eb86abf67
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-09-28 09:57:50 -0500

    Added the Api Path Variables package

---
+++ src/main/java/com/pubg/api/commons/constants/ApiPathVariables.java
@@ -0,0 +1,6 @@
+package com.pubg.api.commons.constants;
+
+public class ApiPathVariables {
+    public static final String V1_ROUTE = "/v1"; // Base route for API version 1
+    public static final String PUBG_ROUTE = "/pubg"; // Route specific to pubg-related endpoints
+}
