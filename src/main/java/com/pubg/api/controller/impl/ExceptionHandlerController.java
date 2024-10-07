dbba5a161bb9a89b4956f69bc284f4baa6fe640f
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-06 19:07:25 -0500

    Updated H2 DB to store in a file

--- src/main/java/com/pubg/api/controller/impl/ExceptionHandlerController.java
+++ src/main/java/com/pubg/api/controller/impl/ExceptionHandlerController.java
@@ -1,7 +1,6 @@
 package com.pubg.api.controller.impl;
 
 import com.pubg.api.commons.dto.ErrorResponse;
-import com.pubg.api.commons.exceptions.WeaponException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,14 +9,4 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j // Lombok annotation for logging
 public class ExceptionHandlerController {
-    @ExceptionHandler(value = {WeaponException.class}) // Handles GameException
-    ResponseEntity<ErrorResponse> handleError(WeaponException weaponException){
-        log.error("New Exception", weaponException); // Log the exception details
-        var errorResponse = ErrorResponse.builder()
-                .codeStatus(Integer.valueOf(weaponException.getHttpStatus(weaponException.getMessage()))) // Set HTTP status code in the response
-                .message(weaponException.getMessage()) // Set the error message in the response
-                .build();
-
-        return ResponseEntity.status(weaponException.getHttpStatus(errorResponse.getMessage())).body(errorResponse); // Return the error response
-    }
 }
