62625ee6317a35f6b3f936b164112c525c9d72bc
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-09 17:29:52 -0500

    Implemented ExceptionHandlerController

--- src/main/java/com/pubg/api/controller/impl/ExceptionHandlerController.java
+++ src/main/java/com/pubg/api/controller/impl/ExceptionHandlerController.java
@@ -1,6 +1,7 @@
 package com.pubg.api.controller.impl;
 
 import com.pubg.api.commons.dto.ErrorResponse;
+import com.pubg.api.commons.exceptions.GlobalExceptionHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,4 +10,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j // Lombok annotation for logging
 public class ExceptionHandlerController {
+
+    @ExceptionHandler(value = {GlobalExceptionHandler.class}) // Handles GameException
+    ResponseEntity<ErrorResponse> handleError(GlobalExceptionHandler globalExceptionHandler) {
+
+        log.error("New Exception", globalExceptionHandler); // Log the exception details
+        var errorResponse = ErrorResponse.builder()
+                .codeStatus(globalExceptionHandler.getHttpStatus().value()) // Set HTTP status code in the response
+                .message(globalExceptionHandler.getMessage()) // Set the error message in the response
+                .build();
+
+        return ResponseEntity.status(globalExceptionHandler.getHttpStatus()).body(errorResponse); // Return the error response
+
+    }
 }
