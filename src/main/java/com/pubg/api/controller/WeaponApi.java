2297a800c6c05548e7ac687ca322a092a4026a6a
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-10 12:23:10 -0500

    Implemented WeaponApi Interface

--- src/main/java/com/pubg/api/controller/WeaponApi.java
+++ src/main/java/com/pubg/api/controller/WeaponApi.java
@@ -1,4 +1,16 @@
 package com.pubg.api.controller;
 
+import com.pubg.api.commons.constants.ApiPathVariables;
+import com.pubg.api.commons.entities.Weapon;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(ApiPathVariables.V1_ROUTE + ApiPathVariables.PUBG_ROUTE)
 public interface WeaponApi {
+
+    @PostMapping("/weapon")
+    ResponseEntity<Weapon> saveWeapon(String id, @RequestBody Weapon weapon);
+
 }
