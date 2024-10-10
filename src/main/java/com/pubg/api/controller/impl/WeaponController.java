800488acde6fb96d9eecfa5b2365c7c8d2e9130d
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-10 12:25:13 -0500

    Implemented WeaponController

--- src/main/java/com/pubg/api/controller/impl/WeaponController.java
+++ src/main/java/com/pubg/api/controller/impl/WeaponController.java
@@ -1,4 +1,25 @@
 package com.pubg.api.controller.impl;
 
-public class WeaponController {
+import com.pubg.api.commons.entities.Weapon;
+import com.pubg.api.controller.WeaponApi;
+import com.pubg.api.services.WeaponService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WeaponController implements WeaponApi {
+
+    private final WeaponService weaponService;
+
+    public WeaponController(WeaponService weaponService) {
+        this.weaponService = weaponService;
+    }
+
+    @Override
+    public ResponseEntity<Weapon> saveWeapon(String id, @RequestBody Weapon weapon) {
+        System.out.println(id);
+        var weaponCreated = this.weaponService.saveWeapon(id, weapon);
+        return ResponseEntity.ok(weaponCreated);
+    }
 }
