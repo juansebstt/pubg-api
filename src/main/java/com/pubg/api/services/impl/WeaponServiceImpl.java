3dca47b62b261ae0c2bd76a4ad3f045d1a07a908
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-10 12:28:02 -0500

    Implemented WeaponServiceImpl

--- src/main/java/com/pubg/api/services/impl/WeaponServiceImpl.java
+++ src/main/java/com/pubg/api/services/impl/WeaponServiceImpl.java
@@ -1,4 +1,29 @@
 package com.pubg.api.services.impl;
 
-public class WeaponServiceImpl {
+import com.pubg.api.commons.entities.Weapon;
+import com.pubg.api.commons.exceptions.GlobalExceptionHandler;
+import com.pubg.api.repositories.WeaponRepository;
+import com.pubg.api.services.WeaponService;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class WeaponServiceImpl implements WeaponService {
+
+    private final WeaponRepository weaponRepository;
+
+    public WeaponServiceImpl(WeaponRepository weaponRepository) {
+        this.weaponRepository = weaponRepository;
+    }
+
+    @Override
+    public Weapon saveWeapon(String id, Weapon weaponRequest) {
+        
+        weaponRequest.setId(Long.parseLong(id));
+        return Optional.of(weaponRequest)
+                .map(weaponRepository::save)
+                .orElseThrow(() -> new GlobalExceptionHandler(HttpStatus.BAD_REQUEST));
+    }
 }
