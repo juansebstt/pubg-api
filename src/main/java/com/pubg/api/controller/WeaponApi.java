10fb785f389c373ffc7d06c392df1bd400e46aed
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-11 11:55:29 -0500

    Updated WeaponApi

--- src/main/java/com/pubg/api/controller/WeaponApi.java
+++ src/main/java/com/pubg/api/controller/WeaponApi.java
@@ -3,14 +3,17 @@ package com.pubg.api.controller;
 import com.pubg.api.commons.constants.ApiPathVariables;
 import com.pubg.api.commons.entities.Weapon;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping(ApiPathVariables.V1_ROUTE + ApiPathVariables.PUBG_ROUTE)
 public interface WeaponApi {
 
     @PostMapping("/weapon")
     ResponseEntity<Weapon> saveWeapon(@RequestBody Weapon weapon);
+
+    @GetMapping("/weapons")
+    ResponseEntity<List<Weapon>> getWeaponsByName(@RequestParam String name);
 
 }
