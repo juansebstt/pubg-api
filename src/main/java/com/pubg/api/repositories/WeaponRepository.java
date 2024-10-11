006560232b6d037c0d5546ff957001154121ca6e
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-11 11:42:09 -0500

    Updated WeaponRepository

--- src/main/java/com/pubg/api/repositories/WeaponRepository.java
+++ src/main/java/com/pubg/api/repositories/WeaponRepository.java
@@ -3,5 +3,10 @@ package com.pubg.api.repositories;
 import com.pubg.api.commons.entities.Weapon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WeaponRepository extends JpaRepository<Weapon, Long> {
+
+    Optional<Weapon> findByNameIgnoreCase(String name);
+
 }
