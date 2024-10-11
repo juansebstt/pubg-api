16ceec7df24112395fe6897b32a9a331e7056456
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-11 11:49:54 -0500

    Updated WeaponService

--- src/main/java/com/pubg/api/services/WeaponService.java
+++ src/main/java/com/pubg/api/services/WeaponService.java
@@ -2,8 +2,16 @@ package com.pubg.api.services;
 
 import com.pubg.api.commons.entities.Weapon;
 
+import java.util.Optional;
+
+
 public interface WeaponService {
+
 
     Weapon saveWeapon(Weapon weaponRequest);
 
+
+    Optional<Weapon> findWeaponByName(String name);
+
 }
+
